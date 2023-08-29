5c073215d6aba2b75621019b1a9d0490b0e151a6
AlanCui4080 <me@alancui.cc>
2023-08-29 21:29:47 +0800

    add: socketstream - basic abstraction of socket

---
+++ src/whim_exception.hpp
@@ -0,0 +1,30 @@
+// This file a part of WhimAP
+// Copyright (C) 2023 WhimAP Authors
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#pragma once
+#include <stdexcept>
+#include <exception>
+#include <system_error>
+
+#include <cstring>
+namespace whimap
+{
+    class network_failure : public std::system_error
+    {
+        public:
+        network_failure(decltype(errno) v,const char* s)
+        : system_error(std::error_code(v,std::generic_category()),s){};
+    };
+} // namespace whimap
