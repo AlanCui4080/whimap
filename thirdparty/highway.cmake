4e9044ecc7d4685092d3746e13f1fa36ba83b494
AlanCui4080 <me@alancui.cc>
2023-09-01 01:36:38 +0800

    disable: contrib of highway

--- thirdparty/highway.cmake
+++ thirdparty/highway.cmake
@@ -1,3 +1,4 @@
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway/hwy)
 set(HWY_ENABLE_TESTS false)
+set(HWY_ENABLE_CONTRIB false)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway)
