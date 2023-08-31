bab327f78a45a2bb7fb6dd3b9b14372f2ea1df6a
AlanCui4080 <me@alancui.cc>
2023-09-01 03:04:47 +0800

    fix: add hwy configs to cache

--- thirdparty/highway.cmake
+++ thirdparty/highway.cmake
@@ -1,4 +1,4 @@
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway/hwy)
-set(HWY_ENABLE_TESTS OFF)
-set(HWY_ENABLE_CONTRIB OFF)
+set(HWY_ENABLE_TESTS OFF CACHE BOOL "Highway Tests")
+set(HWY_ENABLE_CONTRIB OFF CACHE BOOL "Highway Contrib")
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway)
