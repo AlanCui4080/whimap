ce2ef3a32af79a99bda975f1d0b3db54b5ca6392
AlanCui4080 <me@alancui.cc>
2023-09-01 01:44:43 +0800

    fix: from false to OFF

--- thirdparty/highway.cmake
+++ thirdparty/highway.cmake
@@ -1,4 +1,4 @@
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway/hwy)
-set(HWY_ENABLE_TESTS false)
-set(HWY_ENABLE_CONTRIB false)
+set(HWY_ENABLE_TESTS OFF)
+set(HWY_ENABLE_CONTRIB OFF)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/highway)
