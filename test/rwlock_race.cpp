dd3cf4dc4ee37097bfe9303f5f7030b44888540b
AlanCui4080 <me@alancui.cc>
2023-09-01 16:41:04 +0800

    fix: unmatched value in test rwlock_race

--- test/rwlock_race.cpp
+++ test/rwlock_race.cpp
@@ -8,13 +8,13 @@
 #include <vector>
 int main()
 {
-    whimap::rwlock            lock;
-    std::barrier              lch1(16);
-    std::barrier              lch2(16);
-    std::vector<long>         a(512);
+    whimap::rwlock           lock;
+    std::barrier             lch1(16);
+    std::barrier             lch2(16);
+    std::vector<long>        a(512);
     std::vector<std::thread> init_writer;
-    std::mutex                mx2, mx1;
-    std::vector<long>         r1, r2;
+    std::mutex               mx2, mx1;
+    std::vector<long>        r1, r2;
     std::vector<std::thread> reader;
     std::vector<std::thread> writer;
     std::vector<std::thread> reader2;
@@ -27,7 +27,6 @@ int main()
                 lock.lock(whimap::rwlock::WRITE);
                 std::for_each(a.begin(), a.end(), [](long& v) { v = 10; });
                 lock.unlock(whimap::rwlock::WRITE);
-                exit(EXIT_SUCCESS);
             }));
 
     for (size_t i = 0; i < 14; i++)
@@ -38,8 +37,7 @@ int main()
                 std::lock_guard lg(lock);
                 long            sum = std::accumulate(a.begin(), a.end(), 0);
                 std::lock_guard lmg(mx1);
-                r2.push_back(sum);
-                exit(EXIT_SUCCESS);
+                r1.push_back(sum);
             }));
     for (size_t i = 0; i < 2; i++)
         writer.emplace_back(std::thread(
@@ -49,7 +47,6 @@ int main()
                 lock.lock(whimap::rwlock::WRITE);
                 std::for_each(a.begin(), a.end(), [](long& v) { v = 20; });
                 lock.unlock(whimap::rwlock::WRITE);
-                exit(EXIT_SUCCESS);
             }));
     for (size_t i = 0; i < 14; i++)
         reader2.emplace_back(std::thread(
@@ -60,11 +57,27 @@ int main()
                 long            sum = std::accumulate(a.begin(), a.end(), 0);
                 std::lock_guard lmg(mx2);
                 r2.push_back(sum);
-                exit(EXIT_SUCCESS);
             }));
-
-    if (!((std::accumulate(r1.begin(), r1.end(), 0) == 5120) &&
-        (std::accumulate(r2.begin(), r2.end(), 0) == 10240)))
+    for (auto&& v : init_writer)
+    {
+        v.join();
+    }
+    for (auto&& v : reader)
+    {
+        v.join();
+    }
+    for (auto&& v : writer)
+    {
+        v.join();
+    }
+    for (auto&& v : reader2)
+    {
+        v.join();
+    }
+    std::cerr << "R1:" << std::accumulate(r1.begin(), r1.end(), 0) << std::endl;
+    std::cerr << "R2:" << std::accumulate(r2.begin(), r2.end(), 0) << std::endl;
+    if (!((std::accumulate(r1.begin(), r1.end(), 0) == 71680) &&
+          (std::accumulate(r2.begin(), r2.end(), 0) == 143360)))
     {
         exit(EXIT_FAILURE);
     }
